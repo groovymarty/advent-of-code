3158cf4b426ab888599c029faa4d130eb2327039
Marty Sauser <msauser2@gmail.com>
2022-12-01 16:53:21 -0500

    Add comments

--- 2022/day-01/top3.py
+++ 2022/day-01/top3.py
@@ -10,13 +10,16 @@ with open('input.txt') as f:
             curcal += int(line)
         else:
             if curcal >= maxcal:
+                # new greatest, demote old greatest to 2nd greatest, and old 2nd greatest to third
                 maxcal3 = maxcal2
                 maxcal2 = maxcal
                 maxcal = curcal
             elif curcal >= maxcal2:
+                # new second greatest, demote old 2nd greatest to 3rd then save new 2nd
                 maxcal3 = maxcal2
                 maxcal2 = curcal
             elif curcal >= maxcal3:
+                # new third greatest, just save it
                 maxcal3 = curcal
             curcal = 0
 print(maxcal + maxcal2 + maxcal3)
