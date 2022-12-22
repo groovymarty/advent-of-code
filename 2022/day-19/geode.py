361807e008847500484583b5cf6586d2f50a441c
Marty Sauser <msauser2@gmail.com>
2022-12-22 09:56:53 -0500

    Hardcode strategy for first example

--- 2022/day-19/geode.py
+++ 2022/day-19/geode.py
@@ -75,10 +75,16 @@ def make_resources(blueprint, resources_wanted, state, path):
         can_make_extra = can_make_robots(blueprint, extra_resources)
         print(f"  min {minute(time_left)}: {path}{resources} {robots} can make {get_names(can_make)}, extra: {get_names(can_make_extra)}")
         # degree of freedom is here.. need to search for best answer...
-        if len(can_make_extra) > 0:
-            # for now use simple strategy of making lowest numbered robot using extra resources
+        make_me = None
+        if GEODE in can_make:
+            make_me = GEODE
+        elif OBSIDIAN in can_make and robots[OBSIDIAN] < 2:
+            make_me = OBSIDIAN
+        elif CLAY in can_make and robots[CLAY] < (3 if robots[OBSIDIAN] == 0 else 4):
+            make_me = CLAY
+        if make_me:
             state = (resources, robots, time_left)
-            state = make_robot(blueprint, can_make_extra[0], (0, 0, 0, 0), state, path)
+            state = make_robot(blueprint, make_me, (0, 0, 0, 0), state, path)
             if state is None:
                 raise IndexError("Failed to make robot even though required resources were available!")
             resources, robots, time_left = state
@@ -110,7 +116,7 @@ def make_robot(blueprint, robot_wanted, resources_committed, state, path):
 initial_state = ([0, 0, 0, 0], [1, 0, 0, 0], 24)
 
 for i, blueprint in enumerate(blueprints):
-    state = make_resources(blueprint, (0, 0, 0, 1), initial_state, "")
+    state = make_resources(blueprint, (0, 0, 0, 9 if i == 0 else 12), initial_state, "")
     if state is None:
         print(f"{i + 1}: None")
     else:
