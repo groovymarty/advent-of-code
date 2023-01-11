9e7923d07f141a8e9011492acbc1d3f77fb81819
Marty Sauser <msauser2@gmail.com>
2023-01-11 10:17:29 -0500

    Check in best version for part one

--- 2022/day-19/geode.py
+++ 2022/day-19/geode.py
@@ -1,3 +1,10 @@
+# this version gets the correct answer for the part one, but not for part two
+# you can run this version for part two using the commented out values below (time_to_play, max_blueprints),
+# but it won't get the right answer
+# this version does an exhaustive search using three tricks (commented below) to limit the recursion.
+# unfortunately the tricks, and others I tried, don't work for part two
+# will try another approach in geode 2
+
 import re
 
 ORE = 0
@@ -11,15 +18,21 @@ blueprints = []
 blueprint = None
 blueprint_num = 0
 obsidian_needed = 0
-# this version gets the correct answer for the part one, but not for part two
-# you can run this version for part two using the commented out values below, but it won't get the right answer
-# I think both trick #1 and #2 are not applicable for part 2 (32 rounds instead of 24), but if you take them
-# out the exhaustive search takes too long.  Will try another approach in geode2.
-time_to_play = 24  # 32
-max_blueprints = 999  # 3
+time_to_play = 24 #32
+max_blueprints = 999 #3
 max_geode_time_left = 0
 
-with open('example.txt') as f:
+# an array that accumulates the quantity of a given resource at the maximum theoretically possible rate
+# assume you start with zero and you make a new robot for that resource each minute
+# the first minute you have 0 resources and 0 robots, the next minute you have 0 resources and 1 robot,
+# then the robot makes 1 resource plus you get another robot so you have 1 resource and 2 robots,
+# then 3 and 3, 6 and 4, etc.  The array gives only the resource quantity which is 0, 0, 1, 3, 6, etc.
+projected_resource = [0] + [t for t in range(0, time_to_play)]
+for i in range(1, len(projected_resource)):
+    projected_resource[i] += projected_resource[i-1]
+print(projected_resource)
+
+with open('input.txt') as f:
     lines = f.readlines()
     for line in lines:
         phrases = line.strip().split(".")
@@ -90,62 +103,77 @@ class State:
         self.path += ","
         self.time_left -= 1
 
+    def evaluate(self):
+        return self.resources[GEODE]
+
     def get_minute(self):
         return time_to_play - self.time_left + 1
+
+    def projected_obsidian(self, t):
+        t = max(t, 0)
+        return self.resources[OBSIDIAN] + (self.robots[OBSIDIAN] * t) + projected_resource[t]
 
 
 def play(state):
     global max_geode_time_left
-    num_geodes_max = 0
-    # must have at least 2 minutes left or no point making any robots
-    # also don't take longer to make first geode robot than best time found so far (trick #1)
+    best_result = state
+    best_score = -1
+    # must have at least 2 minutes left or no point making robots
+    # if we don't have a geode robot in this state, don't bother going forward in time
+    # any farther than the point in time when we successfully made one on another path (trick #1).
+    # also don't go any farther if it's impossible to make enough obsidian to make another geode robot (trick #2).
     if state.time_left > 1 and \
-            (state.robots[GEODE] or state.time_left >= max_geode_time_left):
-        #print(f"{state.get_minute()}: {state.path}")
+        (state.robots[GEODE] or state.time_left >= max_geode_time_left) and \
+        (state.projected_obsidian(state.time_left - 2) >= obsidian_needed):
+        # print(f"{state.get_minute()}: {state.path}")
         # try making each type of robot, higher-value robots first
-        any_robot = False
         for robot in range(GEODE, -1, -1):
             if state.can_make_robot(robot):
-                any_robot = True
                 # keep track of earliest point in time when a geode robot was made
                 if robot == GEODE and state.time_left >= max_geode_time_left:
                     max_geode_time_left = state.time_left
                     print(f"[{blueprint_num}] {state.get_minute()}: {state.path} making geode robot")
                 try_state = State(state)
                 try_state.make_robot(robot)
-                num_geodes_max = max(num_geodes_max, play(try_state))
-                # if geode robot just made, don't consider other possibilities (trick #2)
+                result = play(try_state)
                 if robot == GEODE:
-                    return num_geodes_max
-        # also try letting time pass without making a robot
-        if not any_robot:
-            try_state = State(state)
-            try_state.pass_time()
-            num_geodes_max = max(num_geodes_max, play(try_state))
-        return num_geodes_max
-    else:
-        # not gonna bother making any more robots, run out the clock with the existing robots
-        return state.resources[GEODE] + state.robots[GEODE] * state.time_left
+                    # no way to improve on making a geode robot when it is possible to make one (trick #3).
+                    return result
+                result_score = result.evaluate()
+                if result_score > best_score:
+                    best_score = result_score
+                    best_result = result
+
+    # also try letting time pass without making a robot
+    if state.time_left > 0:
+        try_state = State(state)
+        try_state.pass_time()
+        result = play(try_state)
+        result_score = result.evaluate()
+        if result_score > best_score:
+            best_result = result
+    return best_result
 
 
-num_geodes_results = []
+results = []
 max_geode_time_lefts = []
 quality_total = 0
 product = 1
 for i, blueprint in enumerate(blueprints[0: max_blueprints]):
     blueprint_num = i + 1
     print(f"doing blueprint {blueprint_num}:")
+    obsidian_needed = blueprint[GEODE][OBSIDIAN]
     state = State()
     state.set_initial_state()
     max_geode_time_left = 0
-    num_geodes = play(state)
-    num_geodes_results.append(num_geodes)
+    result = play(state)
+    results.append(result)
     max_geode_time_lefts.append(max_geode_time_left)
-    print("got", num_geodes)
-    quality_total += num_geodes * blueprint_num
-    product *= num_geodes
+    print("got", result.evaluate())
+    quality_total += result.evaluate() * (i + 1)
+    product *= result.evaluate()
 
-print("geodes", num_geodes_results)
+print("geodes", [result.resources[GEODE] for result in results])
 print("m_g_t_l", max_geode_time_lefts)
 print("quality", quality_total)
 print("product", product)
